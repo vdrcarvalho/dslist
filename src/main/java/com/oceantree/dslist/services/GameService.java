2a381a57d9d4d3b6745d00eade61c84f1c9f89d5
Victor Carvalho <victordrcarvalho@gmail.com>
2023-05-10 23:48:03 -0300

    GET game by id

--- src/main/java/com/oceantree/dslist/services/GameService.java
+++ src/main/java/com/oceantree/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.oceantree.dslist.dto.GameDTO;
 import com.oceantree.dslist.dto.GameMinDTO;
 import com.oceantree.dslist.entities.Game;
 import com.oceantree.dslist.repositories.GameRepository;
@@ -14,8 +16,16 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
