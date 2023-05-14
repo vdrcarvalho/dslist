166d104eff3690634d4487e57855d417c3472a4c
Victor Carvalho <victordrcarvalho@gmail.com>
2023-05-14 18:14:07 -0300

    GET games by list

--- src/main/java/com/oceantree/dslist/services/GameService.java
+++ src/main/java/com/oceantree/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.oceantree.dslist.dto.GameDTO;
 import com.oceantree.dslist.dto.GameMinDTO;
 import com.oceantree.dslist.entities.Game;
+import com.oceantree.dslist.projections.GameMinProjection;
 import com.oceantree.dslist.repositories.GameRepository;
 
 @Component
@@ -27,5 +28,11 @@ public class GameService {
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
 		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
